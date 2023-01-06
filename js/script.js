c54742e78b0960fd218e736ac03deafc225f8633
IharRubanik <igor.rubanik@gmail.com>
2023-01-07 03:57:58 +0400

    1024

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
- const mediaQuery = window.matchMedia('only screen and (max-width: 600px)');
+document.addEventListener("DOMContentLoaded", function () {
+  const mediaQuery = window.matchMedia("only screen and (max-width: 600px)");
   // scroll
   const smoothLinks = document.querySelectorAll('a[href^="#"]');
   for (let smoothLink of smoothLinks) {
@@ -13,19 +13,18 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
-  // const letsSwim = document.querySelector('.main-button'); 
+  // const letsSwim = document.querySelector('.main-button');
   // letsSwim.addEventListener('click', function(e) {
   //   e.preventDefault();
-  //   window.scrollTo({ 
+  //   window.scrollTo({
   //     top: document.querySelector('#beginning').offsetTop,
   //     behavior: "smooth"
   //   })
   // })
 
-
   // const letsSwim = document.querySelector('.main-button');
   // let elemScroll;
-  
+
   // if (mediaQuery.matches) {
   //   letsSwim.href = ('#beginning__right-block');
   //   elemScroll = document.querySelector('#beginning__right-block')
@@ -42,7 +41,6 @@ document.addEventListener("DOMContentLoaded", function() {
   //   })
 
   // })
- 
 
   let circlBackground = document.querySelector(".circl-background");
   let circl2 = document.querySelector(".top");
@@ -55,31 +53,28 @@ document.addEventListener("DOMContentLoaded", function() {
   let sliderImages2 = document.querySelector(".slider-images2");
   let sliderImages3 = document.querySelector(".slider-images3");
   let rotate = 0;
- 
 
   circl2.onclick = function () {
     sliderText2.classList.add("active");
     sliderText1.classList.remove("active");
     sliderText3.classList.remove("active");
-    sliderImages2.classList.add('active');
-    sliderImages1.classList.remove('active');
-    sliderImages3.classList.remove('active');
+    sliderImages2.classList.add("active");
+    sliderImages1.classList.remove("active");
+    sliderImages3.classList.remove("active");
     if (this.classList.contains("top")) {
       if (mediaQuery.matches) {
-        rotateMinus()
-      }
-      else{
-        rotatePlus()
+        rotateMinus();
+      } else {
+        rotatePlus();
       }
       this.classList.replace("top", "active");
       circl1.classList.replace("active", "left");
       circl3.classList.replace("left", "top");
     } else if (this.classList.contains("left")) {
       if (mediaQuery.matches) {
-        rotatePlus()
-      }
-      else{
-        rotateMinus()
+        rotatePlus();
+      } else {
+        rotateMinus();
       }
       this.classList.replace("left", "active");
       circl3.classList.replace("active", "top");
@@ -90,25 +85,23 @@ document.addEventListener("DOMContentLoaded", function() {
     sliderText1.classList.add("active");
     sliderText2.classList.remove("active");
     sliderText3.classList.remove("active");
-    sliderImages1.classList.add('active');
-    sliderImages2.classList.remove('active');
-    sliderImages3.classList.remove('active');
+    sliderImages1.classList.add("active");
+    sliderImages2.classList.remove("active");
+    sliderImages3.classList.remove("active");
     if (this.classList.contains("left")) {
       if (mediaQuery.matches) {
-        rotatePlus()
-      }
-      else{
-        rotateMinus()
+        rotatePlus();
+      } else {
+        rotateMinus();
       }
       this.classList.replace("left", "active");
       circl2.classList.replace("active", "top");
       circl3.classList.replace("top", "left");
     } else if (this.classList.contains("top")) {
       if (mediaQuery.matches) {
-        rotateMinus()
-      }
-      else{
-        rotatePlus()  
+        rotateMinus();
+      } else {
+        rotatePlus();
       }
       this.classList.replace("top", "active");
       circl2.classList.replace("left", "top");
@@ -119,25 +112,23 @@ document.addEventListener("DOMContentLoaded", function() {
     sliderText3.classList.add("active");
     sliderText2.classList.remove("active");
     sliderText1.classList.remove("active");
-    sliderImages3.classList.add('active');
-    sliderImages2.classList.remove('active');
-    sliderImages1.classList.remove('active');
+    sliderImages3.classList.add("active");
+    sliderImages2.classList.remove("active");
+    sliderImages1.classList.remove("active");
     if (this.classList.contains("left")) {
       if (mediaQuery.matches) {
-        rotatePlus()
-      }
-      else{
-        rotateMinus()
+        rotatePlus();
+      } else {
+        rotateMinus();
       }
       this.classList.replace("left", "active");
       circl2.classList.replace("top", "left");
       circl1.classList.replace("active", "top");
     } else if (this.classList.contains("top")) {
       if (mediaQuery.matches) {
-        rotateMinus()
-      }
-      else{
-        rotatePlus()  
+        rotateMinus();
+      } else {
+        rotatePlus();
       }
       this.classList.replace("top", "active");
       circl2.classList.replace("active", "left");
@@ -160,10 +151,10 @@ document.addEventListener("DOMContentLoaded", function() {
   let buttonPrev = document.querySelector(".button-prev");
 
   function nextSlide() {
-    buttonPrev.classList.remove('active')
+    buttonPrev.classList.remove("active");
     for (let i = 0; i < slides.length; i++) {
-      if(i == slides.length - 2){
-        buttonNext.classList.add('active')
+      if (i == slides.length - 2) {
+        buttonNext.classList.add("active");
       }
       if (
         slides[i].classList.contains("active") &&
@@ -171,19 +162,16 @@ document.addEventListener("DOMContentLoaded", function() {
       ) {
         slides[i + 1].classList.replace("centr", "active");
         slides[i].classList.replace("active", "none");
-        
+
         if (slides[i + 2]) {
           slides[i + 2].classList.replace("left", "centr");
-      
         }
         if (slides[i + 3]) {
           slides[i + 3].classList.add("left");
           slides[i + 3].classList.remove("none-reverce");
-          
         }
         if (slides[i - 1]) {
           slides[i - 1].classList.remove("centr");
-          
         }
         break;
       }
@@ -191,7 +179,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function prevSlide() {
-    buttonNext.classList.remove('active')
+    buttonNext.classList.remove("active");
     for (let i = 0; i < slides.length; i++) {
       if (
         slides[i].classList.contains("active") &&
@@ -200,7 +188,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (slides[i - 1]) {
           slides[i - 1].classList.replace("none", "active");
           slides[i].classList.replace("active", "centr");
-          
+
           if (slides[i + 1]) {
             slides[i + 1].classList.replace("centr", "left");
           }
@@ -208,9 +196,9 @@ document.addEventListener("DOMContentLoaded", function() {
             slides[i + 2].classList.replace("left", "none-reverce");
           }
         }
-          if(i == 1){
-        buttonPrev.classList.add('active')
-      }
+        if (i == 1) {
+          buttonPrev.classList.add("active");
+        }
         break;
       }
     }
@@ -295,135 +283,195 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function fadeTransition() {
-
     // Beginning
-    const beginningSection = document.querySelector('#beginning'),
-        beginningContainer = document.querySelector('.beginning__container'),
-        beginningBg      = document.querySelector('.beginning-background');
+    const beginningSection = document.querySelector("#beginning"),
+      beginningContainer = document.querySelector(".beginning__container"),
+      beginningBg = document.querySelector(".beginning-background");
 
     let mintWaterwayHeight = 0,
-    mintWaterwayScroll = 0;
+      mintWaterwayScroll = 0;
 
-    // Waterway 
-    const mintWaterwaySection = document.querySelector('#mintwaterway'),
-      waterwaySection = document.querySelector('#waterway'),
-      waterwayTabs = document.querySelectorAll('.waterway-tabs a'),
-      waterwayText = document.querySelector('.waterway-texts'),
-      waterwayTab = document.querySelectorAll('.waterway-tab');
+    // Waterway
+    const mintWaterwaySection = document.querySelector("#mintwaterway"),
+      waterwaySection = document.querySelector("#waterway"),
+      waterwayTabs = document.querySelectorAll(".waterway-tabs a"),
+      waterwayText = document.querySelector(".waterway-texts"),
+      waterwayTab = document.querySelectorAll(".waterway-tab");
+      axolots = document.querySelectorAll('.axolots')
 
-    if (screen.width >= 600) {
-      let mintWaterwayPercScroll = 0,
-      num = 0,
-      numOld = 0;
 
-      waterwayTabs.forEach((el,i) => el.addEventListener('click', function(e) {
-      e.preventDefault();
+      // начала нового скрипта
+    if (screen.width <= 1280 & screen.width >= 601) {
+      waterwaySection.className = 'first'
+      
+      waterwayTabs.forEach((el, i) =>
+      el.addEventListener("click", function (e) {
+        e.preventDefault();
 
-      waterwayTab.forEach(el => el.classList.remove('active'));
-      waterwayTab[i].classList.add('active');
+        waterwayTab.forEach((el) => el.classList.remove("active"));
+        waterwayTab[i].classList.add("active");
+        if(i == 0) {
+          waterwaySection.className = 'first'
+        }
+        else if (i == 1) {
+          waterwaySection.className = 'second'
+        }
+        else if (i == 2) {
+          waterwaySection.className = 'third'
+        }
+        else if (i == 3) {
+          waterwaySection.className = 'four'
+        }
 
-      const range = document.querySelector(`#waterway-text-${i+1}`).offsetTop - waterwayText.offsetTop - 1;
+        axolots.forEach((el) => el.classList.remove("active"));
+        axolots[i].classList.add("active");
 
-      window.scrollTo({
-      top: mintWaterwaySection.offsetTop + (mintWaterwaySection.clientHeight/6) * (i + 2),
+        const range =
+          document.querySelector(`#waterway-text-${i + 1}`).offsetTop -
+          waterwayText.offsetTop -
+          1;
+
+        waterwayText.scrollTo({
+          top: range,
+          behavior: "smooth",
+        });
       })
+    );
+    }
+      // Конец нового скрипта
 
-      waterwayText.scrollTo({
-      top: range,
-      behavior: "smooth"
-      });
 
-      }))
+    if (screen.width >= 1281) {
+      let mintWaterwayPercScroll = 0,
+        num = 0,
+        numOld = 0;
+
+      waterwayTabs.forEach((el, i) =>
+        el.addEventListener("click", function (e) {
+          e.preventDefault();
+
+          waterwayTab.forEach((el) => el.classList.remove("active"));
+          waterwayTab[i].classList.add("active");
+
+          const range =
+            document.querySelector(`#waterway-text-${i + 1}`).offsetTop -
+            waterwayText.offsetTop -
+            1;
+
+          window.scrollTo({
+            top:
+              mintWaterwaySection.offsetTop +
+              (mintWaterwaySection.clientHeight / 6) * (i + 2),
+          });
+
+          waterwayText.scrollTo({
+            top: range,
+            behavior: "smooth",
+          });
+        })
+      );
 
       let beginningOpacity = 0;
 
-      window.addEventListener('scroll', function() {
+      window.addEventListener("scroll", function () {
+        mintWaterwayPercScroll =
+          (-1 * mintWaterwaySection.getBoundingClientRect().y) /
+          (mintWaterwaySection.clientHeight / 100);
+        beginningPercScroll =
+          (-1 * beginningSection.getBoundingClientRect().y) /
+          (beginningSection.clientHeight / 100);
 
-      mintWaterwayPercScroll = (-1 * mintWaterwaySection.getBoundingClientRect().y/ (mintWaterwaySection.clientHeight/100));
-      beginningPercScroll = (-1 * beginningSection.getBoundingClientRect().y/ (beginningSection.clientHeight/100));
+        mintWaterwayHeight = mintWaterwaySection.clientHeight;
+        mintWaterwayScroll = window.pageYOffset - mintWaterwaySection.offsetTop;
 
-      mintWaterwayHeight = mintWaterwaySection.clientHeight;
-      mintWaterwayScroll = window.pageYOffset - mintWaterwaySection.offsetTop;
+        mintWaterwayScroll < 3 * (mintWaterwayHeight / 6) - 300
+          ? (num = 1)
+          : mintWaterwayScroll < 4 * (mintWaterwayHeight / 6) - 300
+          ? (num = 2)
+          : mintWaterwayScroll < 5 * (mintWaterwayHeight / 6) - 300
+          ? (num = 3)
+          : (num = 4);
 
-      mintWaterwayScroll < 3 * (mintWaterwayHeight / 6) - 300 ?
-      (num = 1) :
-      mintWaterwayScroll < 4 * (mintWaterwayHeight / 6) - 300 ?
-      (num = 2) :
-      mintWaterwayScroll < 5 * (mintWaterwayHeight / 6) - 300 ?
-      (num = 3) : (num = 4);
+        if (beginningPercScroll < 26) {
+          beginningOpacity = 1;
+        } else if (beginningPercScroll >= 26 && beginningPercScroll <= 46) {
+          beginningOpacity = (46 - beginningPercScroll) / 20;
+          mintWaterwayOpacity = (-1 * (28 - beginningPercScroll)) / 20;
 
-      if (beginningPercScroll < 26) {
-      beginningOpacity = 1;
-      } else if (beginningPercScroll >= 26 && beginningPercScroll <= 46) {
+          if (beginningOpacity <= 0.05) {
+            beginningOpacity = 0;
+          }
+          if (beginningOpacity >= 0.95) {
+            beginningOpacity = 1;
+          }
+        } else if (beginningPercScroll > 46) {
+          beginningOpacity = 0;
+        }
 
-      beginningOpacity = (46 - beginningPercScroll)/20;
-      mintWaterwayOpacity = (-1 * (28 - beginningPercScroll)/20);
+        beginningOpacity > 0
+          ? (beginningSection.style.zIndex = 9)
+          : (beginningSection.style.zIndex = -1);
+        beginningSection.style.opacity = beginningOpacity;
 
-      if (beginningOpacity <= 0.05) {
-        beginningOpacity = 0;
-      }
-      if (beginningOpacity >= 0.95) {
-        beginningOpacity = 1;
-      }
+        if (beginningPercScroll > 10) {
+          beginningBg.style.transform = `scale(${
+            Math.round(beginningPercScroll) / 10
+          })`;
+        } else {
+          beginningBg.style.transform = `scale(1)`;
+        }
 
-      } else if (beginningPercScroll > 46) {
-      beginningOpacity = 0;
-      }
+        if (beginningPercScroll > 14) {
+          beginningContainer.classList.add("remove");
+        } else {
+          beginningContainer.classList.remove("remove");
+        }
 
-      beginningOpacity > 0 ? beginningSection.style.zIndex = 9 : beginningSection.style.zIndex = -1;
-      beginningSection.style.opacity = beginningOpacity;
+        if (mintWaterwayPercScroll < 10) {
+          document.querySelector(
+            ".mint-test"
+          ).style.transform = `translate(${0}vw,0)`;
+        } else if (
+          mintWaterwayPercScroll >= 10 &&
+          mintWaterwayPercScroll <= 30
+        ) {
+          document.querySelector(".mint-test").style.transform = `translate(${
+            -((mintWaterwayPercScroll - 10) / 20) * 100
+          }vw,0)`;
+        } else if (mintWaterwayPercScroll > 30) {
+          document.querySelector(
+            ".mint-test"
+          ).style.transform = `translate(${-100}vw,0)`;
+        }
 
-      if (beginningPercScroll > 10) {
-      beginningBg.style.transform = `scale(${Math.round(beginningPercScroll)/10})`;
-      } else {
-      beginningBg.style.transform = `scale(1)`;
-      }
+        if (num === 1) {
+          waterwaySection.classList = "";
+          waterwaySection.classList.add("first");
+        }
+        if (num === 2) {
+          waterwaySection.classList = "";
+          waterwaySection.classList.add("second");
+        }
+        if (num === 3) {
+          waterwaySection.classList = "";
+          waterwaySection.classList.add("third");
+        }
+        if (num === 4) {
+          waterwaySection.classList = "";
+          waterwaySection.classList.add("four");
+        }
 
-      if (beginningPercScroll > 14) {
-      beginningContainer.classList.add('remove')
-      } else {
-      beginningContainer.classList.remove('remove')
-      }
-
-      if (mintWaterwayPercScroll < 10) {
-      document.querySelector('.mint-test').style.transform = `translate(${0}vw,0)`
-      } else if (mintWaterwayPercScroll >= 10 && mintWaterwayPercScroll <= 30) {
-
-      document.querySelector('.mint-test').style.transform = `translate(${-((mintWaterwayPercScroll - 10)/20) * 100}vw,0)`
-      } else if (mintWaterwayPercScroll > 30) {
-      document.querySelector('.mint-test').style.transform = `translate(${-100}vw,0)`
-      }
-
-      if (num === 1) {
-        waterwaySection.classList = '';
-        waterwaySection.classList.add('first');
-      }
-      if (num === 2) {
-        waterwaySection.classList = '';
-        waterwaySection.classList.add('second');
-      }
-      if (num === 3) {
-        waterwaySection.classList = '';
-        waterwaySection.classList.add('third');
-      }
-      if (num === 4) {
-        waterwaySection.classList = '';
-        waterwaySection.classList.add('four');
-      }
-
-      if (num !== numOld) {
-        numOld = num;
-        waterwayTabs[num - 1].click();
-      }
-
-      })
+        if (num !== numOld) {
+          numOld = num;
+          waterwayTabs[num - 1].click();
+        }
+      });
     }
-
   }
   fadeTransition();
   // Accordion
-  const accordionItem = document.querySelectorAll('.accordion-item'),
-        accordionTitle = document.querySelectorAll('.accordion-title');
+  const accordionItem = document.querySelectorAll(".accordion-item"),
+    accordionTitle = document.querySelectorAll(".accordion-title");
 
   function accordionItemSize() {
     for (i = 0; i < accordionTitle.length; i++) {
@@ -433,26 +481,23 @@ document.addEventListener("DOMContentLoaded", function() {
   accordionItemSize();
 
   for (i = 0; i < accordionTitle.length; i++) {
-    accordionTitle[i].addEventListener('click', toggleItem, false);
+    accordionTitle[i].addEventListener("click", toggleItem, false);
   }
 
   function toggleItem() {
     let itemClass = this.parentNode.className;
     for (i = 0; i < accordionItem.length; i++) {
-      accordionItem[i].className = 'accordion-item';
-      accordionTitle[i].className = 'accordion-title';
+      accordionItem[i].className = "accordion-item";
+      accordionTitle[i].className = "accordion-title";
     }
-    if (itemClass == 'accordion-item') {
-      this.parentNode.className = 'accordion-item show';
-      this.closest('.accordion-title').classList.add('show');
+    if (itemClass == "accordion-item") {
+      this.parentNode.className = "accordion-item show";
+      this.closest(".accordion-title").classList.add("show");
     }
   }
 
-  window.addEventListener('resize', function() {
+  window.addEventListener("resize", function () {
     accordionItemSize();
     fadeTransition();
   });
-
 });
-
-
